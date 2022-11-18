b6fdec17432ffd31978c39211d2e6d9abd3fed8b
WilliamWJD <william.jose.wjd@gmail.com>
2022-11-18 09:46:25 -0300

    feat: criando componente header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Header } from './components/Header';
+import { GlobalStyles } from './styles/GlobalStyles';
+
 export function App() {
   return (
-    <h1>Hello WaiterApp - Web</h1>
+    <>
+      <GlobalStyles />
+      <Header />
+    </>
   );
 }
